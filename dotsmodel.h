cbccd3c05c09d51220a175d5d34312b44bd7ec18
jeffrunningit <jeffrey.suen.yw@gmail.com>
2025-02-24 18:19:06 +0100

    Initial commit

---
+++ dotsmodel.h
@@ -0,0 +1,88 @@
+#include <cmath>
+#include <random>
+#include <vector>
+#include <iostream>
+
+#define PI 3.141592
+#define BOXSIZE 30
+#define DOTVEL 0.05
+
+using namespace std;
+
+static double getRandomDouble(double min, double max) {
+    static random_device rd;
+    static mt19937 gen(rd());
+    uniform_real_distribution<double> dis(min, max);
+    return dis(gen);
+}
+
+class Dot {
+public:
+    double x, y, vx, vy;
+    int state; // 0: Susceptible, 1: Infected, 2: Recovered
+    int infected_no = 0;
+    int infected_days = 0;
+
+    
+
+    Dot() { // initialize random position and velocity
+        x = getRandomDouble(0, BOXSIZE);
+        y = getRandomDouble(0, BOXSIZE);
+        double randangle = getRandomDouble(0, 2 * PI);
+        vx = DOTVEL * sin(randangle);
+        vy = DOTVEL * cos(randangle);
+        state = 0;
+    }
+
+    void move() {
+        x += vx;
+        y += vy;
+
+        // Boundary conditions
+        if (x < 0 || x > BOXSIZE) vx *= -1;
+        if (y < 0 || y > BOXSIZE) vy *= -1;
+
+        if (state == 1) infected_days++;
+    }
+};
+
+class DotArray {
+public:
+    vector<Dot> dots;
+    int susceptible = 0, infected = 0, recovered = 0;
+
+    DotArray(int N) {
+        dots.reserve(N);
+        for (int i = 0; i < N; ++i) {
+            dots.emplace_back();
+        }
+        dots[0].state = 1; // Start with one infected dot
+    }
+
+    void update(double infection_radius, double infection_probability, int infection_duration) {
+        for (Dot& dot : dots) {
+            dot.move();
+        }
+
+        for (int i = 0; i < dots.size(); i++) {
+            if (dots[i].state == 2) continue;
+
+            for (int j = i + 1; j < dots.size(); j++) {
+                if (dots[j].state + dots[i].state != 1) continue;
+                // Distance check + probability check
+                double dx = dots[i].x - dots[j].x; // distance check
+                double dy = dots[i].y - dots[j].y;
+                if (dx * dx + dy * dy <= infection_radius * infection_radius && getRandomDouble(0, 1) < infection_probability) {
+                    dots[j].state = 1;  // infected
+                }
+                
+            }
+            
+        }
+
+        for (Dot& dot : dots) {
+            if (dot.state == 1 && dot.infected_days > infection_duration)
+                dot.state = 2;
+        }
+    }
+};
---
+++ sir_simulation.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include "dotsmodel.h"
+
+using namespace std;
+
+int main() {
+    double infection_radius = 1, infection_probability = 0.5;
+    int infection_duration = 10, N = 10000;
+
+    srand(time(nullptr));
+
+	cout << "-------------------------------" << endl;
+	cout << "Default parameters:" << endl;
+	cout << "Size of the box = 30 x 30" << endl;
+	cout << "Infection radius = " << infection_radius << endl;
+	cout << "Infection probability = " << infection_probability << endl;
+	cout << "Infection duration = " << infection_duration << endl;
+	cout << "-------------------------------" << endl;
+	cout << "Use default parameters? (Enter/n)" << endl;
+	string input;
+	getline(cin, input);
+	if (input != ""){
+		cout << "Enter infection radius: ";
+		while (!(cin >> infection_radius)) {
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << "No input provided. Enter again: ";
+		}
+		cout << "Enter infection probability (0 to 1): ";
+		while (!(cin >> infection_probability)) {
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << "No input provided. Enter again: ";
+		}
+		cout << "Enter infection duration: ";
+		while (!(cin >> infection_duration)) {
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << "No input provided. Enter again: ";
+		}
+	};
+
+    ofstream file("results/SIRsimData.csv");
+	if (!file) {
+        cerr << "Error: Could not open the file!" << endl;
+        return 1;
+    }
+	file << "day,susceptible,infected,recovered\n";
+
+    DotArray population(N);
+	int susceptible = N-1, infected = 1, recovered = 0;
+	cout << "Day " << 0 << " | Susceptible: " << susceptible
+             << " | Infected: " << infected
+             << " | Recovered: " << recovered << endl;
+	bool ongoing = true;
+
+    for (int day = 1; day < 1000; ++day) { // Simulate until everyone infected has recovered
+        population.update(infection_radius, infection_probability, infection_duration);
+        
+        // Output counts
+        susceptible = 0, infected = 0, recovered = 0;
+        for (const auto& dot : population.dots) {
+            if (dot.state == 0) susceptible++;
+            else if (dot.state == 1) infected++;
+            else recovered++;
+        }
+		
+        cout << "Day " << day << " | Susceptible: " << susceptible
+             << " | Infected: " << infected
+             << " | Recovered: " << recovered << endl;
+
+		file << day << "," << susceptible << "," << infected << "," << recovered << endl;
+
+		if (infected == 0){
+			ongoing = false;
+			break;
+		}
+    }
+
+	file.close();
+
+	cout << "Simulation finished!" << endl;
+	cout << "Susceptible: " << susceptible
+             << " | Infected: " << infected
+             << " | Recovered: " << recovered << endl;
+
+	if (ongoing){
+		cout << "Pandemic still ongoing after 1000 days. (Infected > 0)" << endl;
+	}
+	cout << float((infected + recovered)) / N * 100 << "% of the population has been infected." << endl;
+
+    return 0;
+}
